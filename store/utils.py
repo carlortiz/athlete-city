55b709a9aaac0921ee3e02a42a817a1f8447db49
carlortiz <mr.carlortiz@gmail.com>
2023-04-19 10:16:03 -0700

    fixed spacing between functions

--- store/utils.py
+++ store/utils.py
@@ -2,6 +2,7 @@
 
 from .models import Customer, Product, Order, OrderItem, ShippingAddress
 
+
 def get_or_create_order_item(product_name, order_items, order):
     order_item = order_items.filter(product__name=product_name).first()
     if not order_item:
@@ -9,6 +10,7 @@ def get_or_create_order_item(product_name, order_items, order):
         order_item = OrderItem.objects.create(order=order, product=product, quantity=1)
     
     return order_item
+
 
 def update_order_item_quantity(action, order_item):
     if action == "add":
