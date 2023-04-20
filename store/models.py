0535d3745f973389f550ded5ba5863e9b70692b9
carlortiz <mr.carlortiz@gmail.com>
2023-04-20 14:07:51 -0700

    created test for process order view

--- store/models.py
+++ store/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=200)
