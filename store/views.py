9c9900fc208904ee223c3e11f5add8866d46cec0
carlortiz <mr.carlortiz@gmail.com>
2024-05-14 21:39:02 -0700

    fixed issue: TypeError: Field 'id' expected a number but got <SimpleLazyObject: <django.contrib.auth.models.AnonymousUser object at 0x00000174E7624920>>

--- store/views.py
+++ store/views.py
@@ -3,41 +3,39 @@ from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from .models import Customer, Order, Product, OrderItem
 from .utils import get_or_create_order_item, update_order_item_quantity
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import AnonymousUser
 
 # Create your views here
 
-
 def store(request):
     products = Product.objects.all()
-    customer, created = Customer.objects.get_or_create(user=request.user)
+    customer, created = Customer.objects.get_or_create(user=request.user if request.user.is_authenticated else None)
     order, created = Order.objects.get_or_create(customer=customer, completed=False)
 
     context = { 'products': products, 'customer': customer, 'order': order } 
     return render(request, 'store/store.html', context)
 
-
 def cart(request):
-    customer, created = Customer.objects.get_or_create(user=request.user)
+    customer, created = Customer.objects.get_or_create(user=request.user if request.user.is_authenticated else None)
     order, created = Order.objects.get_or_create(customer=customer, completed=False)
 
-    context = { 'customer': customer, 'order': order}
+    context = { 'customer': customer, 'order': order }
     return render(request, 'store/cart.html', context)
 
-
 def checkout(request):
-    customer, created = Customer.objects.get_or_create(user=request.user)
+    customer, created = Customer.objects.get_or_create(user=request.user if request.user.is_authenticated else None)
     order, created = Order.objects.get_or_create(customer=customer, completed=False)
 
-    context = { 'customer': customer, 'order': order}
+    context = { 'customer': customer, 'order': order }
     return render(request, 'store/checkout.html', context)
-
 
 def update_item(request):
     data = json.loads(request.body)
     product_name = data.get('product_name')
     action = data.get('action')
 
-    customer, created = Customer.objects.get_or_create(user=request.user)
+    customer, created = Customer.objects.get_or_create(user=request.user if request.user.is_authenticated else None)
     order, created = Order.objects.get_or_create(customer=customer, completed=False)
     order_items = order.orderitem_set.all()
 
@@ -53,9 +51,8 @@ def update_item(request):
     }
     return JsonResponse(data)
 
-
 def process_order(request):
-    customer, created = Customer.objects.get_or_create(user=request.user)
+    customer, created = Customer.objects.get_or_create(user=request.user if request.user.is_authenticated else None)
     order, created = Order.objects.get_or_create(customer=customer, completed=False)
 
     order.completed = True
